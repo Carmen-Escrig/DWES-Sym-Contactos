52dcb9f5971717ce400951ecb17644e77b9a2f8e
alu0557 <aluesc0557@ieselcaminas.org>
2023-10-24 11:53:01 +0200

    Antes (poco) de empezar con el reto

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Contacto;
+
 
 class PageController extends AbstractController
 {
@@ -17,9 +21,12 @@ class PageController extends AbstractController
     }
 
     #[Route('/', name: 'inicio')]
-    public function inicio(): Response
+    public function inicio(ManagerRegistry $doctrine): Response
     {
+        $repositorio = $doctrine->getRepository(Contacto::class);
+        $contactos = $repositorio->findAll();
         return $this->render('index.html.twig',[
+            "contactos" =>$contactos
         ]);
     }
 }
